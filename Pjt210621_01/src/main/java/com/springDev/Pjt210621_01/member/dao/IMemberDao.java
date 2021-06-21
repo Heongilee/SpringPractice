8db1cf6c152a471315c80ca4cd555c54c99a64a7
Heongilee <gjsrl1@gmail.com>
2021-06-21 10:48:23 +0900

    210621 10:48

---
+++ Pjt210621_01/src/main/java/com/springDev/Pjt210621_01/member/dao/IMemberDao.java
@@ -0,0 +1,10 @@
+package com.springDev.Pjt210621_01.member.dao;
+
+import com.springDev.Pjt210621_01.member.Member;
+
+public interface IMemberDao {
+	int memberInsert(Member member);
+	Member memberSelect(Member member);
+	int memberUpdate(Member member);
+	int memberDelete(Member member);
+}
